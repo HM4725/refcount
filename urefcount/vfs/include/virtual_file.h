38fcbd29a28cf3562646ddc9b6c9a3c4b0456f24
hm4725 <4725hmc@gmail.com>
2023-04-05 16:39:48 +0900

    feat(virtual_file): capacity[private->protected]

--- urefcount/vfs/include/virtual_file.h
+++ urefcount/vfs/include/virtual_file.h
@@ -8,34 +8,36 @@
 
 class virtual_file {
  private:
+  // Private Types
   static constexpr size_t BLOCK_SZ = 4096;
   static constexpr size_t BLOCK_DEFAULT_NUM = 4;
   struct alignas(BLOCK_SZ) block {
     char d[BLOCK_SZ];
     block() { memset(d, 0, BLOCK_SZ); }
   };
-
+  // Private Fields
   int fd;
-  size_t capacity;  // The number of blocks
   struct block *cache;
-
- private:
+  // Private Methods
   ssize_t read_block(void *buf, size_t nbytes, off_t offset);
 
  protected:
+  // Protected Types
   static constexpr size_t CACHE_LINE_SIZE = 64;
   struct alignas(CACHE_LINE_SIZE) page {
     volatile int _refcount;
     bool cached;
     page(): _refcount(0), cached(false) {}
   };
-
+  // Protected Fields
+  size_t capacity;  // The number of blocks
   struct page *pages;
-
+  // Protected Methods
   virtual int ref(off_t bn);
   virtual int unref(off_t bn);
 
  public:
+  // Public Methods
   virtual_file(const char *fname);
   ~virtual_file();
   ssize_t read(void *buf, size_t nbytes, off_t offset);
