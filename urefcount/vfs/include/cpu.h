11a5402338582f0407ca5d214c13f113d60e3e06
hm4725 <4725hmc@gmail.com>
2023-04-27 00:29:25 +0900

    build(cpu.h): portability
    - Allow apple to compile but it doesn't work correctly.

--- urefcount/vfs/include/cpu.h
+++ urefcount/vfs/include/cpu.h
@@ -1,7 +1,7 @@
-// _GNU_SOURCE
 #ifndef __CPU_H__
 #define __CPU_H__
 
+#ifdef __linux__
 #include <pthread.h>
 #include <sched.h>
 
@@ -13,5 +13,10 @@ inline void set_cpuid(int id) {
   CPU_SET(id, &cpuset);
   pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
 }
+#else  // __APPLE__, _WIN32
+#warning "Must run on Linux to use get_cpuid and set_cpuid correctly."
+inline int get_cpuid() { return 0; }
+inline void set_cpuid(int id) { (void)0; }
+#endif
 
 #endif /* __CPU_H__ */
