bca32892722a438ffc8f4c10a40a0f7cb141a6d4
hm4725 <4725hmc@gmail.com>
2023-09-10 12:14:35 +0000

    test(krefcount): add user-level syscall test

---
+++ krefcount/test/syscalls.h
@@ -0,0 +1,16 @@
+#ifndef __SYSCALLS_H__
+#define __SYSCALLS_H__
+
+#include <unistd.h>
+
+#define __NR_DISTRIBUTE_REFCOUNT 451
+#define __NR_CENTRALIZE_REFCOUNT 452
+
+static inline long distribute_refcount(int fd, loff_t offset, size_t count) {
+	return syscall(__NR_DISTRIBUTE_REFCOUNT, fd, offset, count);
+}
+static inline long centralize_refcount(int fd, loff_t offset, size_t count) {
+	return syscall(__NR_CENTRALIZE_REFCOUNT, fd, offset, count);
+}
+
+#endif /* __SYSCALLS_H__ */
